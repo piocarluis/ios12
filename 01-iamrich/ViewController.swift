2bc8030de2b8a0a0ab72c1bef62fdcb4f10ab5f1
Luis Adrian <piocarluis@hotmail.com>
2019-01-01 20:23:50 -0500

    Initial Commit

---
+++ 01-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Luis Adrian Carpio Toro on 1/1/19.
+//  Copyright © 2019 Luis Adrian Carpio Toro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
